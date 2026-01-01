0c7e071ef0069b4befad7fb118d950b3e1f96b70
Rafid <129318321+rafidabrar47@users.noreply.github.com>
2026-01-01 17:59:58 +0600

    implement database connection (T-01)
    * database connection class (T-01)
    
    * fix: forcing database file push

---
+++ config/Database.php
@@ -0,0 +1,25 @@
+<?php
+// Connection file
+
+class Database {
+    private $host = "localhost";
+    private $db_name = "scheduler_db";
+    private $username = "root";
+    private $password = ""; 
+    public $conn;
+
+    // Method to get the database connection
+    public function getConnection() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
+?>
